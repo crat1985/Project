d1ffdc76256286b7819401833fec6760427faa47
RIC217 <melericcardo9400@gmail.com>
2022-10-16 11:23:29 +0200

    First version of navigator window

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-console.log("Hello World");
+const {app,BrowserWindow} = require("electron")
+const path = require("path")
+const https = require("https")
+
+function createWindow(){
+    const w = new BrowserWindow({
+        width: 1080,
+        height: 720,
+        webPreferences: {
+            preload: path.join(__dirname,"preload.js")
+        }
+    })
+
+    w.loadFile("index.html")
+}
+
+app.whenReady().then(()=>{
+    createWindow()
+
+    app.on("activate",()=>{
+        if (BrowserWindow.getAllWindows.length===0){
+            createWindow()
+        }
+    })
+})
+
+app.on("window-all-closed",()=>{
+    if (process.platform!=='darwin'){
+        app.quit()
+    }
+})
+
+let engine = "duckduckgo"
+let search = "vscode"
+let lang = "fr-fr"
+https.get("https://serpapi.com/search.json?engine="+engine+"&q="+search+"&kl="+lang,function(res){
+    let data = ""
+
+    res.on("data",buff=>{
+        data+=buff
+    })
+
+    res.on("end",()=>{
+        console.log(JSON.parse(data));
+    })
+})

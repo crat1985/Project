2a1223b15a60564b60cb1e6ec3e60ab6d25eb015
RIC217 <melericcardo9400@gmail.com>
2022-10-23 21:02:28 +0200

    Added custom page URL

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
 window.addEventListener("DOMContentLoaded",()=>{
     const webview = document.querySelector("webview")
     const urlBar = document.querySelector("input[type=text]")
-    webview.addEventListener("did-start-loading",()=>{
-        window.api.sendUrl(webview.getURL(),webview.getTitle())
-        urlBar.value = webview.src
-    })
+    const homePage = webview.src
     webview.addEventListener("dom-ready",()=>{
-        window.api.sendUrl(webview.getURL(),webview.getTitle())
-        urlBar.value = webview.src
+        let url = webview.getURL()
+        let title = webview.getTitle()
+        if(url===homePage){
+            url = "navigator://new-tab"
+        }
+        window.api.sendUrl(url,title)
+        if(webview.getURL()===homePage){
+            urlBar.value = "navigator://new-tab"
+        } else {
+            urlBar.value = webview.src
+        }
+        webview.addEventListener("did-start-loading",()=>{
+            window.api.sendUrl(webview.getURL(),webview.getTitle())
+            if(webview.getURL()===homePage){
+                urlBar.value = "navigator://new-tab"
+            } else {
+                urlBar.value = webview.src
+            }
+        })
     })
     window.api.updateURL((event,url)=>{
-        webview.src = url
-        urlBar.value = webview.src
+        if(url==="navigator://new-tab"){
+            webview.src = homePage
+            urlBar.value = "navigator://new-tab"
+        } else{
+            webview.src = url
+            urlBar.value = webview.src
+        }
     })
     urlBar.value = webview.src
     urlBar.addEventListener("keypress",e=>{
         if(e.key=="Enter"){
-            let urlTemp = webview.src.toLowerCase()
-            console.log("a"+urlTemp);
-            if(urlTemp.startsWith("http://")||urlTemp.startsWith("https://")||urlTemp.startsWith("/")||urlTemp.startsWith("C:/")||urlTemp.startsWith("D:/")||urlTemp.startsWith("E:/")){
-                webview.src = url.value
-                console.log('1');
+            let urlTemp = urlBar.value.toLowerCase()
+            if(urlTemp.startsWith("http://")||urlTemp.startsWith("https://")||urlTemp.startsWith("file://")){
+                webview.src = urlBar.value
+            } else if(urlTemp==="navigator://new-tab"){
+                webview.src = homePage
             } else{
-                console.log("2");
                 webview.src = "http://"+urlBar.value
             }
             webview.focus()
